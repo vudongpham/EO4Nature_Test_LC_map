1db43c47a87c39e6a6b669af7737b0006df5090a
vudongpham <vudong.pham27@gmail.com>
2025-06-12 13:50:25 +0200

    test

---
+++ data/convert_to_xyz.py
@@ -0,0 +1,75 @@
+import os
+import rasterio
+import shutil
+import numpy as np
+
+color_dict = {
+    1 : np.array([255, 0, 0]),
+    2 : np.array([34, 139, 34]),
+    3 : np.array([128, 255, 0]),
+    4 : np.array([255, 8, 0]),
+    5 : np.array([128, 64, 0]),
+    6 : np.array([204, 242, 77]),
+    7 : np.array([255, 255, 128]),
+    8 : np.array([255, 128, 255]),
+    9 : np.array([191, 191, 191]),
+    10 : np.array([0, 128, 255]),
+    11 : np.array([0, 255, 255]),
+    254 : np.array([255, 255, 255]),
+    255 : np.array([255, 255, 255])
+}
+
+def toRaster_rgb(source_image, out_image, arr_in):
+    path = source_image
+
+    path_out = out_image
+
+    with rasterio.open(path) as src:
+        band_crs = src.crs
+        band_tranform = src.transform
+
+    meta = {
+        "driver": "GTiff",
+        "height": arr_in.shape[0],
+        "width": arr_in.shape[1],
+        "count": 3,  
+        "dtype": 'uint8',
+        "crs": band_crs,  
+        "transform": band_tranform,
+
+    }
+
+    with rasterio.open(path_out, "w", **meta) as dst:
+        for i in range(3):
+            dst.write(arr_in[..., i], i + 1)
+
+
+def convert_to_rgb(source_image, source_out, color_dict):
+    with rasterio.open(source_image) as src:
+        arr = src.read(1)
+    
+    arr_rgb = np.full(shape=(arr.shape[0], arr.shape[1], 3), fill_value=0, dtype=np.uint8)
+
+    key_values = list(color_dict.keys())
+
+    for k in key_values:
+        mask = arr == k
+        color = color_dict[k]
+        arr_rgb[mask, :] = color
+    toRaster_rgb(source_image, source_out, arr_rgb)
+
+
+if __name__ == '__main__':
+    
+    if os.path.exists("images/image_reprojected.tif"):
+        os.remove("images/image_reprojected.tif")
+
+    source_image = 'images/clc_2021.tif'
+    rgb_out =  'images/clc_2021_rgb.tif'
+    max_zoom = 15
+    nodata='255,255,255'
+    convert_to_rgb(source_image, rgb_out, color_dict)
+
+    shutil.rmtree('images/tiles')
+    os.system(f'gdalwarp -srcnodata {nodata} -dstalpha  -t_srs EPSG:3857 {rgb_out} images/image_reprojected.tif')
+    os.system(f'gdal2tiles --verbose  --processes=4  -z 0-{max_zoom} images/image_reprojected.tif images/tiles/')
---
+++ main.js
@@ -0,0 +1,43 @@
+const max_zoom = 15
+const tile_center = [52.52430,11.03388]
+
+var map = L.map('map', {
+    center: tile_center,
+    zoom: 11,
+    minZoom: 10,  // Minimum zoom level
+    maxZoom: max_zoom   // Maximum zoom level
+});
+
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: max_zoom,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+});
+
+var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+    maxZoom: max_zoom,
+	attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+});
+
+var baseMaps = {
+    "OpenStreetMap": osm,
+    "Esri satellite": Esri_WorldImagery,
+};
+
+Esri_WorldImagery.addTo(map)
+L.control.layers(baseMaps).addTo(map);
+
+tile_map = L.tileLayer('/data/images/tiles/{z}/{x}/{y}.png', {tms:true});
+tile_map.addTo(map);
+tile_map.bringToFront();
+
+
+var slider = document.getElementById('slider');
+var sliderValue = document.getElementById('slider-value');
+
+var slider = document.getElementById('slider');
+var sliderValue = document.getElementById('slider-value');
+
+slider.addEventListener('input', function(e) {
+tile_map.setOpacity(parseInt(e.target.value, 10) / 100)
+sliderValue.textContent = e.target.value + '%';
+});
